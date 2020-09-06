9703e6738d5692b96007a6ca7a1182eaf4b60dc4
Pig Fang <g-plane@hotmail.com>
2020-09-06 23:00:30 +0800

    add tests for C-printf Parser

--- src/c-printf-parser/index.ts
+++ src/c-printf-parser/index.ts
@@ -14,7 +14,7 @@ type ParseControl<I> =
   Char extends keyof ControlsMap ? [ControlsMap[Char], Rest] :
   [false, Rest] : never
 
-type ParsePrintFormat<I, Placeholders extends any[] = []> =
+export type ParsePrintFormat<I, Placeholders extends any[] = []> =
   I extends '' ? Placeholders :
   I extends `${infer Head}${infer Rest}` ?
   Head extends '%' ?
@@ -22,13 +22,3 @@ type ParsePrintFormat<I, Placeholders extends any[] = []> =
   Control extends false ? ParsePrintFormat<Rest, Placeholders> :
   ParsePrintFormat<Rest, [...Placeholders, Control]> : never :
   ParsePrintFormat<Rest, Placeholders> : never
-
-type Example1 = ParsePrintFormat<''>
-type Example2 = ParsePrintFormat<'Any string'>
-type Example3 = ParsePrintFormat<'The result is %d.'>
-type Example4 = ParsePrintFormat<'The result is %%d.'>
-type Example5 = ParsePrintFormat<'The result is %%%d.'>
-type Example6 = ParsePrintFormat<'The result is %f.'>
-type Example7 = ParsePrintFormat<'The result is %h.'>
-type Example8 = ParsePrintFormat<'Hello %s: score is %d.'>
-type Example9 = ParsePrintFormat<'The result is %'>
---
+++ src/c-printf-parser/test.ts
@@ -0,0 +1,14 @@
+import { Equal, Expect } from '@type-challenges/utils'
+import { ParsePrintFormat } from '.'
+
+type Tests = [
+  Expect<Equal<ParsePrintFormat<''>, []>>,
+  Expect<Equal<ParsePrintFormat<'Any string'>, []>>,
+  Expect<Equal<ParsePrintFormat<'The result is %d.'>, ['dec']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %%d.'>, []>>,
+  Expect<Equal<ParsePrintFormat<'The result is %%%d.'>, ['dec']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %f.'>, ['float']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %h.'>, ['hex']>>,
+  Expect<Equal<ParsePrintFormat<'Hello %s: score is %d.'>, ['string', 'dec']>>,
+  Expect<Equal<ParsePrintFormat<'The result is %'>, []>>,
+]
