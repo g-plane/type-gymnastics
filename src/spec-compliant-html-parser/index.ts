5c21360ea5352ca51b708b7a9308060a8ac5e087
Pig Fang <g-plane@hotmail.com>
2020-09-24 10:06:11 +0800

    [spec-compliant-html-parser] update

--- src/spec-compliant-html-parser/index.ts
+++ src/spec-compliant-html-parser/index.ts
@@ -1,6 +1,6 @@
 type Whitespace = '\u0009' | '\u000a' | '\u000c' | '\u000d' | '\u0020'
 type LowerAlpha = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
-type UpperAlpha = `${uppercase LowerAlpha}`
+type UpperAlpha = Uppercase<LowerAlpha>
 type Digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 
 export type TrimStart<S> =
